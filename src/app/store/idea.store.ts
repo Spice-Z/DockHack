980bb932234eb3388697ad9731fa3d70ecd248ce
小倉優悟 <jigenbakdaaan@gmail.com>
2018-05-01 18:52:50 +0900

    [update] apiを修正

--- src/app/store/idea.store.ts
+++ src/app/store/idea.store.ts
@@ -4,25 +4,25 @@ import { Idea } from "../idea";
 
 export interface IIdeaState {
   ideas: Idea[];
-  hereId:number;
+  hereId: number;
 }
 
-export interface IHereId{
-  id:number;
+export interface IHereId {
+  id: number;
 }
 
 export function rootReducer(lastState: IIdeaState, action: Action): IIdeaState {
-  switch(action.type) {
-    case docksActions.MOVE_TO_DEATIL_PAGE: 
-      console.log('MOVE_TO_DEATIL_PAGE');
+  switch (action.type) {
+    case docksActions.MOVE_TO_DEATIL_PAGE:
+      console.log("MOVE_TO_DEATIL_PAGE");
       lastState.hereId = 1;
       return lastState;
     case docksActions.MOVE_TO_IDEA_LINE:
-      console.log('MOVE_TO_IDEA_LINE');    
+      console.log("MOVE_TO_IDEA_LINE");
       lastState.hereId = 1;
       return lastState;
   }
-  console.log('rootReducer is called');
+  console.log("rootReducer is called");
   console.dir(lastState);
   return lastState;
 }
